2c958a474428dc02f2adb83fa1470cfabcc4e9ea
kate <kate@dariox.club>
2024-11-22 16:37:25 +0800

    Start creating code generator for things like FLWidget and FLWindow

---
+++ FLTKSharp.Generator/CodeGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace FLTKSharp.Generator
+{
+    public class CodeGenerator
+    {
+        private readonly RootCodeGenElement _root;
+        public CodeGenerator(RootCodeGenElement root)
+        {
+            _root = root;
+        }
+
+        private static (string, string?)? ParseTagData(string line)
+        {
+            if (!line.StartsWith("--"))
+                return null;
+            var dashIndex = line.IndexOf("--");
+            var start = line.Substring(dashIndex + 2);
+            var sepIndex = start.IndexOf(":");
+            if (sepIndex == -1)
+                return (start, null);
+            var key = start.Substring(0, sepIndex);
+            var val = start.Substring(sepIndex + 1, start.Length - sepIndex - 1);
+            if (string.IsNullOrEmpty(val))
+                return (key, null);
+            return (key, val);
+        }
+    }
+}
---
+++ FLTKSharp.Generator/RootCodeGenElement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FLTKSharp.Generator
+{
+    [XmlRoot("Root")]
+    public class RootCodeGenElement
+    {
+        [XmlElement("Template")]
+        public List<CodeGenTemplateElement> Templates { get; set; } = [];
+    }
+
+    public class CodeGenTemplateElement
+    {
+        [Required]
+        [XmlAttribute("Name")]
+        public string Name { get; set; } = "";
+        [XmlText]
+        public string Text { get; set; } = "";
+    }
+
+    public class CodeGenClassElement
+    {
+        [Required]
+        [XmlAttribute("Name")]
+        public string Name { get; set; } = "";
+        [Required]
+        [XmlAttribute("Prefix")]
+        public string Prefix { get; set; } = "";
+
+        [XmlElement("Extends")]
+        public List<CodeGenClassExtendsElement> Extends { get; set; } = [];
+
+        [XmlElement("Template")]
+        public List<CodeGenInsertTemplateElement> Templates { get; set; } = [];
+    }
+    public class CodeGenClassExtendsElement
+    {
+        [Required]
+        [XmlAttribute("Name")]
+        public string Name { get; set; } = "";
+    }
+
+    public class CodeGenInsertTemplateElement
+    {
+        [Required]
+        [XmlAttribute("ResourceName")]
+        public string ResourceName { get; set; } = "";
+    }
+}
